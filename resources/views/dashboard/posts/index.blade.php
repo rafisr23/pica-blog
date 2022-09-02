c88400c153683c784c866ee7110b16fae5c90cee
Abdur Rafi <rafiridwan23@gmail.com>
2022-09-02 22:16:15 +0700

    update and delete post

--- resources/views/dashboard/posts/index.blade.php
+++ resources/views/dashboard/posts/index.blade.php
@@ -11,6 +11,9 @@
       {{ session('success') }}
       <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
+    {{-- <script>
+      alert('berhasil');
+    </script> --}}
     @endif
     <a href="/dashboard/posts/create" class="btn btn-primary mb-3">Create new post</a>
     <table class="table table-striped table-sm">
@@ -30,12 +33,17 @@
           <td>{{ $post->category->name }}</td>
           <td>
             <a href="/dashboard/posts/{{ $post->slug }}" class="badge bg-info"><span data-feather="eye"></span></a>
-            <a href="" class="badge bg-warning"><span data-feather="edit"></span></a>
-            <a href="" class="badge bg-danger"><span data-feather="x-circle"></span></a>
+            <a href="/dashboard/posts/{{ $post->slug }}/edit" class="badge bg-warning"><span data-feather="edit"></span></a>
+            <form action="/dashboard/posts/{{ $post->slug }}" method="POST" class="d-inline">
+              @method('delete')
+              @csrf
+              <button class="badge bg-danger border-0" onclick="return confirm('Are you sure?')"><span data-feather="x-circle"></span></button>
+            </form>
           </td>
         </tr>
         @endforeach
       </tbody>
     </table>
   </div>
+  
 @endsection
--- resources/views/dashboard/posts/show.blade.php
+++ resources/views/dashboard/posts/show.blade.php
@@ -7,8 +7,12 @@
       <h2 class="mb-3">{{ $post->title }}</h2>
 
       <a href="/dashboard/posts" class="btn btn-success"><i class="bi bi-arrow-left"></i> Back to my posts</a>
-      <a href="" class="btn btn-warning"><i class="bi bi-pencil-square"></i> Edit</a>
-      <a href="" class="btn btn-danger"><i class="bi bi-x-circle"></i> Delete</a>
+      <a href="/dashboard/posts/{{ $post->slug }}/edit" class="btn btn-warning"><i class="bi bi-pencil-square"></i> Edit</a>
+      <form action="/dashboard/posts/{{ $post->slug }}" method="POST" class="d-inline">
+        @method('delete')
+        @csrf
+        <button class="btn btn-danger" onclick="return confirm('Are you sure?')"><i class="bi bi-x-circle"></i> Delete</button>
+      </form>
 
       <img src="https://source.unsplash.com/500x400?{{ $post->category->name }}" class="card-img-top mt-3" alt="{{ $post->category->name }}">
 
